f2a6c1cac4eff0fa7c943c2bfc1c4a5e3848bc2f
Your Name <you@example.com>
2020-06-11 00:14:46 -0300

    Add project files.

---
+++ PruebaAPi/Dapper/UsuariosDALcs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data.SqlClient;
+using System.Data;
+using PruebaAPi.Entities;
+
+namespace PruebaAPi.Dapper
+{
+    public class UsuariosDALcs
+    {
+
+        public static IEnumerable<Roles> Ver()
+        {
+            string sql = "SELECT Id, Descripcion FROM Roles";
+
+            using (var connection = new SqlConnection("Server=NOTEBENJA;Database=dbTest;Trusted_Connection=True;"))
+            {
+                var lista = connection.Query<Roles>(sql).ToList();
+
+                return lista;
+            }
+
+        }
+
+    }
+}
